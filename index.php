f1c0c6b6fb638714fa8a2dc3e4e30359e96653d2
RaihanDrDevs <raihanamol247@gmail.com>
2023-05-16 12:30:43 +0600

    php index file added

---
+++ index.php
@@ -0,0 +1,36 @@
+<h1><?php echo "Learn PHP " ?> </h1>
+<hr>
+<?php ?>
+<hr>
+
+<?php ?>
+<hr>
+<h3>১.০৩ - ভ্যারিয়েবল নিয়ে আরও কথা, কনস্ট্যান্ট এবং কমেন্টস</h3>
+
+
+<hr>
+<h3>১.০২ - ভ্যারিয়েবল পরিচিতি| Variable: $name = "Raihan Ali " , $age = 35 .</h3>
+
+    <h4><i> <u> কোড </u></i></h4> 
+    
+<pre>
+    <code>
+        <?php 
+            echo "Learn PHP in Bangla"
+        ?>
+    </code>
+</pre>
+
+<?php echo "-------------"; ?>
+
+<h4><i> <u> প্রাকটিস </u></i></h4> 
+
+<?php 
+echo "Learn PHP in Bangla";
+echo "Learn PHP in Bangla";
+echo "Learn PHP in Bangla";
+?>
+
+<?php ?>
+
+
